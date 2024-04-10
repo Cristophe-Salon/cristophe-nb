79d32019dae8fe42f3bc1550241bae819ccdb413
Luke Carl Hartman <luke@lukecarlhartman.com>
2024-04-10 14:34:53 -0700

    ada compliance

--- footer.js
+++ footer.js
@@ -73,3 +73,139 @@ LINK: https://www.youtube.com/watch?v=IgpONQwPJYM
 
 	setFolderLinks();
 })();
+
+// ADA Compliance
+
+// Function to set attributes for elements
+function setAriaAttributes(selector, attributeName, attributeValue) {
+	const elements = document.querySelectorAll(selector);
+	elements.forEach((element) => {
+		element.setAttribute(attributeName, attributeValue);
+	});
+}
+
+function setAriaLabelExtLink(selector, attributeName, attributeValue) {
+	const elements = document.querySelectorAll(selector);
+	elements.forEach((element) => {
+		// Get the text content of the element
+		const textContent = element.textContent.trim();
+
+		// Combine the text content with the attributeValue
+		const ariaLabelValue = textContent + " " + attributeValue;
+
+		// Set the aria-label attribute
+		element.setAttribute(attributeName, ariaLabelValue);
+	});
+}
+
+window.addEventListener("load", function () {
+	// ADA: _blank links add aria-label="(opens in a new window)"
+	setAriaLabelExtLink(
+		'a[target="_blank"]',
+		"aria-label",
+		"(opens in a new window)"
+	);
+
+	// ADA: href="tel:9492190920" aria-label
+	setAriaLabelExtLink(
+		'a[href="tel:9492190920"] .sqsrte-text-color--lightAccent',
+		"aria-label",
+		"(opens in a new window)"
+	);
+
+	// ADA: plyr__controls__item video time role="button"
+	setAriaAttributes(".plyr__controls__item", "role", "button");
+
+	// ADA: plyr__controls__item video time tabindex="0"
+	setAriaAttributes(".plyr__controls__item", "tabindex", 0);
+
+	// ADA: plyr__video video time role="button"
+	setAriaAttributes(
+		".native-video-player .plyr__video-wrapper",
+		"role",
+		"button"
+	);
+
+	// ADA: plyr__video video time tabindex="0"
+	setAriaAttributes(
+		".native-video-player .plyr__video-wrapper",
+		"tabindex",
+		0
+	);
+
+	// ADA: plyr__video video time tabindex="0"
+	setAriaAttributes(
+		".native-video-player .plyr__video-wrapper",
+		"aria-label",
+		""
+	);
+
+	// ADA: plyr__video video time role="button"
+	setAriaAttributes(".video-player__controls-blocker", "role", "button");
+
+	// ADA: plyr__video video time tabindex="0"
+	setAriaAttributes(".video-player__controls-blocker", "tabindex", 0);
+
+	// ADA: chevron roles
+	setAriaAttributes(".chevron--right", "role", "presentation");
+
+	// ADA: chevron left chevron--left
+	setAriaAttributes(".chevron--left", "role", "presentation");
+
+	// ADA: hidden svg roles
+	setAriaAttributes(
+		'svg[data-image-mask-id="aa8c1a27f9060ca25a23"]',
+		"role",
+		"presentation"
+	);
+
+	// ADA: newsletter-form-spinner
+	setAriaAttributes(".newsletter-form-spinner", "role", "presentation");
+
+	// ADA: newsletter-form-button-icon
+	setAriaAttributes(".newsletter-form-button-icon", "role", "presentation");
+
+	// ADA: data-usage="social-icons-svg"
+	setAriaAttributes(
+		'svg[data-usage="social-icons-svg"]',
+		"aria-hidden",
+		"true"
+	);
+
+	// ADA: ob-widget-btn__icon
+	setAriaAttributes(".ob-widget-btn__icon", "role", "presentation");
+
+	// ADA: form required aria - email-yui_3_17_2_1_1561059871607_37975-field
+	setAriaAttributes(
+		"#email-yui_3_17_2_1_1561059871607_37975-field",
+		"aria-required",
+		"true"
+	);
+
+	// ADA: href="https://cristophe-newport-beach.myshopify.com" aria-label
+	setAriaAttributes(
+		'a[href="https://cristophe-newport-beach.myshopify.com"]',
+		"aria-label",
+		"Cristophe Shop"
+	);
+
+	// ADA: src="https://phorest.com/book/salons//cristophesalon" iframe aria-label
+	setAriaAttributes(
+		'iframe[src="https://phorest.com/book/salons//cristophesalon"]',
+		"aria-label",
+		"Cristophe Booking"
+	);
+
+	// ADA: src="https://phorest.com/salon/cristophesalon?wmode=opaque"
+	setAriaAttributes(
+		'iframe[src="https://phorest.com/salon/cristophesalon?wmode=opaque"]',
+		"aria-label",
+		"Cristophe Booking"
+	);
+
+	// ADA: icon--cart role
+	setAriaAttributes(".icon--cart", "role", "presentation");
+
+	// ADA: accordion items (.accordion-item__dropdown p) role="heading"
+	setAriaAttributes(".accordion-item__dropdown p", "role", "heading");
+});
